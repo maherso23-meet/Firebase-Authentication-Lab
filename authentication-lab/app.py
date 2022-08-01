4874d19f225c58d15946aa22aadd0b69014fff27
maherso23-meet <maher.soue@gmail.com>
2022-08-01 09:30:59 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -31,6 +31,15 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    try:
+        login_session['user'] = auth.create_user_with_email_and_password(email, password)
+        return redirect(url_for('add_tweet'))
+    except:
+        return render_template("signup.html")
+
     return render_template("signin.html")
 
 
